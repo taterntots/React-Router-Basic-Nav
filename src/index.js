fb6502fc969bde6f1fdd2539efd2e5ecd726c5fa
taterntots <matthew.a.weidner@gmail.com>
2019-11-11 14:51:32 -0500

    Declared my routes in our App.js file.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
